d76d09a67f2a36d76820eaa1ae6fad73099192d1
EwanShilland <e.shilland@ucl.ac.uk>
2024-01-25 12:50:07 +0000

    added a bit more text

--- gitTestScript20240125.R
+++ gitTestScript20240125.R
@@ -1,3 +1,10 @@
 #Git test 25/1/24 in new repo urban-fiesta
 
 #test test test
+
+
+# git pants at pdf files so edit the .gitignore file and add *.pdf
+#can do the same for other file formats and folders eg raw-data/*
+
+#diff tab in git shows what you've changed
+#"more" "revert" goes back a version
